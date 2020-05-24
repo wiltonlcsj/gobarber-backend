b19bb1e04ed6a538430a39c1b4fca218f57bc0f7
Wilton Leonardo <wilton.leonardo@hotmail.com>
2020-05-23 23:33:10 -0300

    Alterações para a versão web

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import path from 'path';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import './database';
@@ -21,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -56,9 +57,19 @@ class UserController {
     if (oldPassword && !(await (await user).checkPassword(oldPassword)))
       return res.status(401).json({ error: 'Password does not match' });
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({ id, name, email, avatar });
   }
 }
 
